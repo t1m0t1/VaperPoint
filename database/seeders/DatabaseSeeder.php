c9e95212ace6717431cf23b1d5dea4faa7a2a85a
Juan Tonelli <tonelli.juanjo@gmail.com>
2025-08-20 21:12:27 -0300

    upgradeo de laravel 10 a laravel 12

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    
+
     /**
      * Seed the application's database.
      */
